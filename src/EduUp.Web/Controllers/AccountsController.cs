31b2cfbed8db7d2872e843bd44c509863958f8ee
zokirjanov <zokirjonovshohruxbek1016@gmail.com>
2023-02-20 11:08:58 +0500

    Some bugs fixed

--- src/EduUp.Web/Controllers/AccountsController.cs
+++ src/EduUp.Web/Controllers/AccountsController.cs
@@ -8,70 +8,63 @@ namespace EduUp.Web.Controllers;
 
 public class AccountsController : Controller
 {
-	private readonly IAccountService _accountService;
+    private readonly IAccountService _service;
 
-	public AccountsController(IAccountService accountService)
-	{
-		this._accountService = accountService;
-	}
+    public AccountsController(IAccountService accountService)
+    {
+            this._service = accountService;
+    }
 
-	[HttpGet("login")]
-	public ViewResult Login()
-	{
-		return View("Login");
-	}
+    [HttpGet("login")]
+    public ViewResult Login() => View("Login");
 
-	[HttpPost("login")]
-	public async Task<IActionResult> LoginAsync(AccountLoginDto accountLoginDto)
-	{
-		if (ModelState.IsValid)
-		{
-			try
-			{
-				string token = await _accountService.AccountLoginAsync(accountLoginDto);
-				HttpContext.Response.Cookies.Append("X-Access-Token", token, new CookieOptions()
-				{
-					HttpOnly = true,
-					SameSite = SameSiteMode.Strict
-				});
-				return RedirectToAction("Index", "Products", new { area = "" });
-			}
-			catch (ModelErrorException modelError)
-			{
-				ModelState.AddModelError(modelError.Property, modelError.Message);
-				return Login();
-			}
-			catch
-			{
-				return Login();
-			}
-		}
-		else return Login();
-	}
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginAsync(AccountLoginDto accountLoginDto)
+    {
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                string token = await _service.AccountLoginAsync(accountLoginDto);
+                HttpContext.Response.Cookies.Append("X-Access-Token", token, new CookieOptions()
+                {
+                    HttpOnly = true,
+                    SameSite = SameSiteMode.Strict
+                });
+                return RedirectToAction("Index", "Products", new { area = "" });
+            }
+            catch (ModelErrorException modelError)
+            {
+                ModelState.AddModelError(modelError.Property, modelError.Message);
+                return Login();
+            }
+            catch
+            {
+                return Login();
+            }
+        }
+        else return Login();
+    }
 
-	[HttpGet("register")]
-	public ViewResult Register()
-	{
-		return View("Register");
-	}
 
-	[HttpPost("register")]
-	public async Task<IActionResult> RegisterAsync(AccountRegisterDto accountRegisterDto)
-	{
-		if (ModelState.IsValid)
-		{
-			bool result = await _accountService.AccountRegisterAsync(accountRegisterDto);
-			if (result)
-			{
-				return RedirectToAction("login", "accounts", new { area = "" });
-			}
-			else
-			{
-				return Register();
-			}
-		}
-		else return Register();
-	}
+    [HttpGet("register")]
+    public ViewResult Register() => View("Register");
+
+    [HttpPost("register")]
+    public async Task<IActionResult> RegisterAsync(AccountRegisterDto accountRegisterDto)
+    {
+        if (ModelState.IsValid)
+        {
+            bool result = await _service.AccountRegisterAsync(accountRegisterDto);
+            if (result)
+            {
+                return RedirectToAction("login", "accounts", new { area = "" });
+            }
+            else
+            {
+                return Register();
+            }
+        }
+        else return Register();
+    }
 }
-
-
--- src/EduUp.Web/Program.cs
+++ src/EduUp.Web/Program.cs
@@ -5,33 +5,27 @@ using Microsoft.EntityFrameworkCore;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddWeb(builder.Configuration);
-builder.Services.AddMemoryCache();
+builder.Services.AddControllersWithViews();
 builder.Services.AddHttpContextAccessor();
-builder.Services.AddScoped<IAccountService, IAccountService>();
+builder.Services.AddWeb(builder.Configuration);
+builder.Services.AddService();
+
 
 string connectionString = builder.Configuration.GetConnectionString("Database");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 
-builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
