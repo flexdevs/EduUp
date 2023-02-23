319046bc00fd9bf8b1964089eb42f1f4d4b14a83
zokirjanov <zokirjonovshohruxbek1016@gmail.com>
2023-02-23 18:39:02 +0500

    Added Layout

--- src/EduUp.Web/Controllers/HomeController.cs
+++ src/EduUp.Web/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 
 namespace EduUp.Web.Controllers
 {
-    [Authorize]
+   
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
