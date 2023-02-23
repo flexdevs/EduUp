d1fcf8ac7c79c7e634ddfedcbeaa9949dd9530b8
A'zamjon Shaydullayev <azamjondev@gmail.com>
2023-02-23 17:27:55 +0500

    updated service | updated coment and rate service

--- src/EduUp.Service/Services/Comments/CommentService.cs
+++ src/EduUp.Service/Services/Comments/CommentService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using EduUp.DataAccess.Interfaces.Common;
+using EduUp.Domain.Entities.Comments;
+using EduUp.Service.Dtos.Comments;
+using EduUp.Service.Interfaces.Comments;
+using EduUp.Service.Interfaces.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,25 @@ using System.Threading.Tasks;
 
 namespace EduUp.Service.Services.Comments
 {
-	internal class CommentService
+	public class CommentService : ICommentService
 	{
+		private readonly IUnitOfWork _unitOfWork;
+		private readonly IIdentityService _identityService;
+
+		public CommentService(IUnitOfWork unitOfWork, IIdentityService identityService)
+		{
+			this._unitOfWork = unitOfWork;
+			this._identityService = identityService;
+		}
+		public async Task<bool> CreateCommentAsync(CreateCommentDto createCommentDto)
+		{
+			Comment comment = (Comment)createCommentDto;
+			comment.UserId = _identityService.Id!.Value;
+
+			_unitOfWork.Comments.Add(comment);
+			var result = await _unitOfWork.SaveChangesAsync();
+
+			return result > 0;
+		}
 	}
 }
